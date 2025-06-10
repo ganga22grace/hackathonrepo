b7a06d3ba6e8a066e7907fa233186a740ceccd06
ganga22grace <hganga.92@gmail.com>
2025-06-10 19:54:52 +0530

    Add files via upload

---
+++ mychaosApp/mychaosApp/src/main/java/com/hackathon/mychaosApp/MychaosAppApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.mychaosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychaosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychaosAppApplication.class, args);
+	}
+
+}
