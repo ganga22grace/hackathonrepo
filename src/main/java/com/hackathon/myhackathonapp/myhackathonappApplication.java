91ead6c9ffa8b386496490279cc17d4c2c428b03
ganga22grace <hganga.92@gmail.com>
2025-06-10 21:25:12 +0530

    Add files via upload

---
+++ src/main/java/com/hackathon/myhackathonapp/myhackathonappApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.myhackathonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myhackathonappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(myhackathonappApplication.class, args);
+	}
+
+}
